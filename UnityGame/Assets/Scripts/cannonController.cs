c85ca1f8647705bbf9ab4e7d6d279f2d0e5e4002
Toni.James <toni.james@autodatacorp.org>
2020-07-02 18:26:23 -0400

    Cannon now aims at player and rotates towards
    Cannon rotates to aim towards player and shoots towards player

--- UnityGame/Assets/Scripts/cannonController.cs
+++ UnityGame/Assets/Scripts/cannonController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class CannonController : MonoBehaviour
@@ -40,6 +41,30 @@ public class CannonController : MonoBehaviour
     // Update is called once per frame, but it is tied to frame rate. 
     void Update()
     {
+        float deg = 0.0f;
+
+        if (shootAtPlayer)
+        {
+
+            float opposite;
+            float ajacent;
+            opposite = player.transform.position.x - transform.position.x;
+            ajacent = player.transform.position.y - transform.position.y;
+            float rot;
+            rot = Mathf.Atan(opposite / ajacent);
+            if (ajacent < 0)
+            {
+                rot = -rot;
+            }
+            else
+            {
+                rot = Mathf.PI/2 + ((Mathf.PI / 2) - rot);
+            }
+            deg = rot * Mathf.Rad2Deg;
+            transform.eulerAngles = new Vector3(0, 0, deg);
+            // transform.Rotate(rota//);
+        }
+
         if (justFired)
         {
 
@@ -58,7 +83,7 @@ public class CannonController : MonoBehaviour
         {
             if (checkDistance())
             {
-                fire();
+                fire(deg);
                 justFired = true;
             }
         }
@@ -78,18 +103,9 @@ public class CannonController : MonoBehaviour
     }
 
 
-    void fire()
+    void fire(float degree)
     {
 
-        if (shootAtPlayer)
-        {
-            // aim at make ball go towards player direction. 
-            // rotate the sprite to look at player. 
-
-            shootingDirection = player.transform.position- transform.position;
-            shootingDirection.Normalize();
-            
-        }
 
         Vector3 iPosition = transform.position;
         if (shootingDirection.y < 0)
@@ -109,7 +125,16 @@ public class CannonController : MonoBehaviour
             iPosition.x = iPosition.x - ARROW_DOWN_OFFSET;
         }
 
+        iPosition = transform.position;
+        if (shootAtPlayer)
+        {
+            // aim at make ball go towards player direction. 
+            // rotate the sprite to look at player. 
 
+            shootingDirection = (player.transform.position - iPosition);
+            shootingDirection.Normalize();
+
+        }
         GameObject arrow = Instantiate(projectilePrefab, iPosition, Quaternion.identity);
         arrow.GetComponent<Rigidbody2D>().velocity = shootingDirection * ARROW_BASE_SPEED; // adjust velocity
         arrow.transform.Rotate(0, 0, Mathf.Atan2(shootingDirection.y, shootingDirection.x) * Mathf.Rad2Deg);
