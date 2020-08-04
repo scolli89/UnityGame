00fd91baa66b653da1abad20c896582b9023341c
Phoenix5050 <59894902+Phoenix5050@users.noreply.github.com>
2020-08-04 16:01:01 -0400

    Main menu now fully functional again
    - any player can navigate main menu
    - Need to add cancel functionality to options menu
    - first player to hit "play" from main menu is automatically assigned as player 1 in setup menu (button press release)

--- UnityGame/Assets/Scripts/PlayerControl/MainMenu.cs
+++ UnityGame/Assets/Scripts/PlayerControl/MainMenu.cs
@@ -2,12 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenu, optionsMenu;
+    public GameObject firstButton, optionsFirstButton, optionsClosedButton;
+
+    void Start(){
+        // set default selected object
+        EventSystem.current.SetSelectedGameObject(firstButton);
+    }
+
+    void Update(){
+        if(optionsMenu.activeSelf==true )//&& InputSystem)
+        {
+            Debug.Log("temp");
+            //CloseOptions();
+        }
+    }
+    
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("PlayerSetup");
+    }
+
+    public void OpenOptions(){
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+        // deselect selected object from previous menu
+        EventSystem.current.SetSelectedGameObject(null);
+        // set default selected object
+        EventSystem.current.SetSelectedGameObject(optionsFirstButton);
+    }
+
+    public void CloseOptions(){
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+        // deselect selected object from previous menu
+        EventSystem.current.SetSelectedGameObject(null);
+        // set default selected object
+        EventSystem.current.SetSelectedGameObject(optionsClosedButton);
     }
 
     public void QuitGame()
